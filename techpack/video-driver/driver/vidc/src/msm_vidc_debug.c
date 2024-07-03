f118b5ab47b6ba98843b6a48aed6603911a3cbbc
Alexander Winkowski <dereference23@outlook.com>
2024-07-03 10:31:59 +0000

    techpack: video: Remove wrong exports
    ERROR: modpost: techpack/video-driver/video/video: 'msm_vidc_lossless_encode' exported twice. Previous export was in techpack/video-driver/msm_video/msm_video.ko
    ERROR: modpost: techpack/video-driver/video/video: 'msm_vidc_syscache_disable' exported twice. Previous export was in techpack/video-driver/msm_video/msm_video.ko
    ERROR: modpost: techpack/video-driver/video/video: 'msm_vidc_fw_dump' exported twice. Previous export was in techpack/video-driver/msm_video/msm_video.ko
    ERROR: modpost: techpack/video-driver/video/video: 'msm_vidc_enable_bugon' exported twice. Previous export was in techpack/video-driver/msm_video/msm_video.ko
    
    Change-Id: Icbdbffa91ab4f971e90496af5c9fe3a8d554afae
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- techpack/video-driver/driver/vidc/src/msm_vidc_debug.c
+++ techpack/video-driver/driver/vidc/src/msm_vidc_debug.c
@@ -211,20 +211,12 @@ module_param_cb(msm_vidc_synx_fence_enable,
 	&msm_vidc_synx_fence_debug_fops, &g_core, 0644);
 
 bool msm_vidc_lossless_encode = !true;
-EXPORT_SYMBOL(msm_vidc_lossless_encode);
-
 bool msm_vidc_syscache_disable = !true;
-EXPORT_SYMBOL(msm_vidc_syscache_disable);
-
 int msm_vidc_clock_voting = !1;
 int msm_vidc_ddr_bw = !1;
 int msm_vidc_llc_bw = !1;
-
 bool msm_vidc_fw_dump = !true;
-EXPORT_SYMBOL(msm_vidc_fw_dump);
-
 unsigned int msm_vidc_enable_bugon = !1;
-EXPORT_SYMBOL(msm_vidc_enable_bugon);
 
 #define MAX_DBG_BUF_SIZE 4096
 
